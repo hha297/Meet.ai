55a0fe461d6f6b538c3775d34a6c6f3f6815e34d
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-06-20 14:04:55 +0300

    Dashboard Navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,17 @@
+import { CommandDialog, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
+import React, { Dispatch } from 'react';
+
+interface DashboardCommandProps {
+        open: boolean;
+        setOpen: Dispatch<React.SetStateAction<boolean>>;
+}
+export const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
+        return (
+                <CommandDialog open={open} onOpenChange={setOpen}>
+                        <CommandInput placeholder="Find a meeting or agent" />
+                        <CommandList>
+                                <CommandItem>T</CommandItem>
+                        </CommandList>
+                </CommandDialog>
+        );
+};
--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -9,15 +9,28 @@ import {
         DropdownMenuSeparator,
         DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+
+import {
+        Drawer,
+        DrawerContent,
+        DrawerDescription,
+        DrawerFooter,
+        DrawerHeader,
+        DrawerTitle,
+        DrawerClose,
+        DrawerTrigger,
+} from '@/components/ui/drawer';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { GeneratedAvatar } from '@/components/generated-avatar';
 import { ChevronRightIcon, CreditCardIcon, LogOutIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { Button } from '@/components/ui/button';
 
 export const DashboardUserButton = () => {
         const router = useRouter();
         const { data, isPending } = authClient.useSession();
-
+        const isMobile = useIsMobile();
         if (isPending || !data?.user) {
                 return null;
         }
@@ -30,6 +43,57 @@ export const DashboardUserButton = () => {
                         },
                 });
         };
+
+        if (isMobile) {
+                return (
+                        <Drawer>
+                                <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/4 hover:bg-white/10 overflow-hidden">
+                                        {data.user.image ? (
+                                                <Avatar>
+                                                        <AvatarImage src={data.user.image} alt={data.user.name} />
+                                                </Avatar>
+                                        ) : (
+                                                <GeneratedAvatar
+                                                        seed={data.user.name}
+                                                        variant="initials"
+                                                        className="size-9 mr-3"
+                                                />
+                                        )}
+                                        <div className="flex flex-col gap-0.5 text-left outline-hidden flex-1 min-w-0">
+                                                <span className="text-sm font-semibold text-white truncate">
+                                                        {data.user.name}
+                                                </span>
+                                                <span className="text-xs text-muted-foreground truncate">
+                                                        {data.user.email}
+                                                </span>
+                                        </div>
+                                        <ChevronRightIcon className="size-4 text-white" />
+                                </DrawerTrigger>
+                                <DrawerContent>
+                                        <DrawerHeader>
+                                                <DrawerTitle className="text-sm font-semibold">
+                                                        {data.user.name}
+                                                </DrawerTitle>
+                                                <DrawerDescription className="text-xs text-muted-foreground">
+                                                        {data.user.email}
+                                                </DrawerDescription>
+                                        </DrawerHeader>
+
+                                        <DrawerFooter className="flex flex-col gap-2">
+                                                <Button variant="outline" onClick={() => {}}>
+                                                        <CreditCardIcon className="size-4 text-black" />
+                                                        Billing
+                                                </Button>
+                                                <Button variant="outline" onClick={onSignOut}>
+                                                        <LogOutIcon className="size-4 text-black" />
+                                                        Sign Out
+                                                </Button>
+                                        </DrawerFooter>
+                                </DrawerContent>
+                        </Drawer>
+                );
+        }
+
         return (
                 <DropdownMenu>
                         <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/4 hover:bg-white/10 overflow-hidden">
