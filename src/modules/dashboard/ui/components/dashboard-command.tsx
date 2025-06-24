3f799272a7ac678a9ad08241094d2080d143600c
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-06-24 09:18:43 +0300

    Responsive Dialog

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,4 +1,4 @@
-import { CommandDialog, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
+import { CommandInput, CommandItem, CommandList, CommandResponsiveDialog } from '@/components/ui/command';
 import React, { Dispatch } from 'react';
 
 interface DashboardCommandProps {
@@ -7,11 +7,11 @@ interface DashboardCommandProps {
 }
 export const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
         return (
-                <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
                         <CommandInput placeholder="Find a meeting or agent" />
                         <CommandList>
                                 <CommandItem>T</CommandItem>
                         </CommandList>
-                </CommandDialog>
+                </CommandResponsiveDialog>
         );
 };
