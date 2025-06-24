dded3ef84de747baae1ba999418f7e6ba05e0fc9
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-06-24 10:09:52 +0300

    Agent create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { TRPCReactProvider } from '@/trpc/client';
+import { Toaster } from 'sonner';
 
 const spaceGrotesk = Space_Grotesk({
         subsets: ['latin'],
@@ -21,7 +22,10 @@ export default function RootLayout({
         return (
                 <TRPCReactProvider>
                         <html lang="en">
-                                <body className={`${spaceGrotesk.className} antialiased`}>{children}</body>
+                                <body className={`${spaceGrotesk.className} antialiased`}>
+                                        <Toaster />
+                                        {children}
+                                </body>
                         </html>
                 </TRPCReactProvider>
         );
---
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -0,0 +1,123 @@
+import { useTRPC } from '@/trpc/client';
+import { AgentGetOne } from '../../type';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { agentsInsertSchema } from '../../schemas';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { GeneratedAvatar } from '@/components/generated-avatar';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
+
+interface AgentFormProps {
+        onSuccess?: () => void;
+        onCancel?: () => void;
+        initialValues?: AgentGetOne;
+}
+
+export const AgentForm = ({ onSuccess, onCancel, initialValues }: AgentFormProps) => {
+        const trpc = useTRPC();
+
+        const queryClient = useQueryClient();
+
+        const createAgent = useMutation(
+                trpc.agents.create.mutationOptions({
+                        onSuccess: async () => {
+                                await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions());
+                                toast.success('Agent created.');
+                                if (initialValues?.id) {
+                                        await queryClient.invalidateQueries(
+                                                trpc.agents.getOne.queryOptions({
+                                                        id: initialValues.id,
+                                                }),
+                                        );
+                                }
+                                onSuccess?.();
+                        },
+                        onError: (error) => {
+                                toast.error(error.message);
+                                // TODO: Check if error is "FORBIDDEN", redirect to login
+                        },
+                }),
+        );
+
+        const form = useForm<z.infer<typeof agentsInsertSchema>>({
+                resolver: zodResolver(agentsInsertSchema),
+                defaultValues: {
+                        name: initialValues?.name ?? '',
+                        instructions: initialValues?.instructions ?? '',
+                },
+        });
+
+        const isEdit = !!initialValues?.id;
+        const isPending = createAgent.isPending;
+
+        const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
+                if (isEdit) {
+                        // TODO: Add edit agent
+                        console.log('Editing agent:', values);
+                } else {
+                        createAgent.mutate(values);
+                }
+        };
+
+        return (
+                <Form {...form}>
+                        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+                                <GeneratedAvatar
+                                        seed={form.watch('name')}
+                                        variant="botttsNeutral"
+                                        className="border size-16"
+                                />
+                                <FormField
+                                        control={form.control}
+                                        name="name"
+                                        render={({ field }) => (
+                                                <FormItem>
+                                                        <FormLabel>Name</FormLabel>
+                                                        <FormControl>
+                                                                <Input {...field} placeholder="Agent Name" />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                </FormItem>
+                                        )}
+                                />
+                                <FormField
+                                        control={form.control}
+                                        name="instructions"
+                                        render={({ field }) => (
+                                                <FormItem>
+                                                        <FormLabel>Instructions</FormLabel>
+                                                        <FormControl>
+                                                                <Textarea
+                                                                        {...field}
+                                                                        placeholder="You are a helpful assistant that can answer questions and help users with their questions."
+                                                                />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                </FormItem>
+                                        )}
+                                />
+                                <div className="flex items-center justify-between gap-x-2">
+                                        {onCancel && (
+                                                <Button
+                                                        type="button"
+                                                        disabled={isPending}
+                                                        variant={'ghost'}
+                                                        onClick={onCancel}
+                                                >
+                                                        Cancel
+                                                </Button>
+                                        )}
+                                        <Button type="submit" disabled={isPending}>
+                                                {isEdit ? 'Update' : 'Create'}
+                                        </Button>
+                                </div>
+                        </form>
+                </Form>
+        );
+};
