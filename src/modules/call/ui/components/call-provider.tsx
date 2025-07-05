3768254c5c96958508d357c9023f6515a40c0f55
hoangha297 <ha.dh290702@gmail.com>
2025-07-05 04:15:01 +0300

    Video Call (Stream)

---
+++ src/modules/call/ui/components/call-provider.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { authClient } from '@/lib/auth-client';
+import { Loader2Icon } from 'lucide-react';
+import { CallConnect } from './call-connect';
+import { generatedAvatarUri } from '@/lib/avatar';
+
+interface CallProviderProps {
+        meetingId: string;
+        meetingName: string;
+}
+const CallProvider = ({ meetingId, meetingName }: CallProviderProps) => {
+        const { data, isPending } = authClient.useSession();
+
+        if (!data || isPending) {
+                return (
+                        <div className="flex h-screen items-center justify-center bg-radial from-sidebar-accent to-sidebar">
+                                <Loader2Icon className="animate-spin size-6 text-white" />
+                        </div>
+                );
+        }
+        return (
+                <CallConnect
+                        meetingId={meetingId}
+                        meetingName={meetingName}
+                        userId={data.user.id}
+                        userName={data.user.name}
+                        userImage={data.user.image ?? generatedAvatarUri({ seed: data.user.name, variant: 'initials' })}
+                />
+        );
+};
+
+export default CallProvider;
